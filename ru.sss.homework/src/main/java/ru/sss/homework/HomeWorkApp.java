94dd688ad15e23608f7ac11e58773004161e0d62
AleshaGlebov2000 <95049987+AleshaGlebov2000@users.noreply.github.com>
2022-02-09 19:50:54 +0300

    Add files via upload

---
+++ ru.sss.homework/src/main/java/ru/sss/homework/HomeWorkApp.java
@@ -0,0 +1,59 @@
+package ru.sss.homework;
+
+import java.util.SimpleTimeZone;
+
+public class HomeWorkApp {
+   /* public static void main(String[] args) {
+        printThreeWords ();
+    }
+
+    public static void printThreeWords () {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }*/
+
+  /*  public static void main(String[] args) {
+        checkSumSign();
+    }
+
+    public static void checkSumSign() {
+        int a = -9;
+        int b = 5;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    } */
+ /* public static void main(String[] args) {
+      printColor();
+  }
+      public static void printColor() {
+          int value = 150;
+
+          if (value <= 0) {
+              System.out.println("Красный");
+          }
+          if (value > 0 && value <= 100) {
+              System.out.println("Жёлтый");
+          }
+          if (value > 100)
+          {
+              System.out.println("Зеленый");
+          }
+      }*/
+
+    /*public static void main(String[] args) {
+        compareNumbers(); }
+    public static void compareNumbers () {
+        int a = 9;
+        int b = 8;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }*/
+}
